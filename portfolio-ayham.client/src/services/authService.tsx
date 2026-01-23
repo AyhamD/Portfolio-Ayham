f086948e07c4f25c990dd90a5d7bdc4c14f3056a
Ayham Darwish <ayhamdarwish1993@gmail.com>
2026-01-23 18:04:33 +0100

    test

--- portfolio-ayham.client/src/services/authService.tsx
+++ portfolio-ayham.client/src/services/authService.tsx
@@ -11,7 +11,7 @@ const api: AxiosInstance = axios.create({
 });
 
 // Add token to requests if available
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config:any) => {
   const token = localStorage.getItem("admin_token");
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
